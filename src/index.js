4df1686ad6f34cb7fad1c37cfe5978bef6f3067d
ValXux <104838245+ValXux@users.noreply.github.com>
2024-04-16 14:22:37 +0000

    es un package

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <h1>Titulo desde react</h1>
+    </StrictMode>
+);
